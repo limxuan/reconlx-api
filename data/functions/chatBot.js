6f3a01ec725f6813f6aea7427f1a14f235483edb
LimXuan7 <olimxuan2005@gmail.com>
2021-01-15 11:03:42 +0800

    v1.0.6 | Changed chatbot api

--- data/functions/chatBot.js
+++ data/functions/chatBot.js
@@ -2,7 +2,7 @@ async function chatBot(message, input) {
     if(!message) throw new ReferenceError('reconlx => "message" is not defined')
     if(!input) throw new ReferenceError('reconlx => "input" is not defined')
     const fetch = require('node-fetch')
-    fetch(`https://pepee.ga/chat?message=${input}`)
+    fetch(`https://bruhapi.xyz/cb/${input}`)
         .then(res => res.json())
         .then(async json => {
             return message.reply(json.response)
