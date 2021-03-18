0522d786e2c3d976bd73bc4b04532242e1994115
LimXuan7 <olimxuan2005@gmail.com>
2021-03-18 19:41:38 +0800

    v1.2.31

--- data/functions/chatBot.js
+++ data/functions/chatBot.js
@@ -1,11 +1,16 @@
 async function chatBot(message, input) {
-    if(!message) throw new ReferenceError('reconlx => "message" is not defined')
-    if(!input) throw new ReferenceError('reconlx => "input" is not defined')
-    const fetch = require('node-fetch')
-    fetch(`https://bruhapi.xyz/cb/${input}`)
-        .then(res => res.json())
-        .then(async json => {
-            return message.reply(json.res)
-        })
+    if (!message)
+        throw new ReferenceError('reconlx => "message" is not defined');
+    if (!input) throw new ReferenceError('reconlx => "input" is not defined');
+    const fetch = require("node-fetch");
+    fetch(
+        `https://api.monkedev.com/fun/chat?msg=${encodeURIComponent(
+            input
+        )}&uid=000`
+    )
+        .then((res) => res.json())
+        .then(async (json) => {
+            return message.reply(json.response);
+        });
 }
 module.exports = chatBot;
