6cf09612d56ea41b6d5889417db2b59e39add999
reconlx <olimxuan2005@gmail.com>
2021-08-10 18:56:20 +0800

    👌 IMPROVE: cleaner exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
+// reconDB
 export { reconDB, reconDBOptions, reconDBSchema } from "./database/reconDB";
 
+// giveaways
 export {
-    GiveawayClient,
     GiveawayClientOptions,
     GiveawayClientSchema,
     StartOptions,
+    GiveawayClient,
 } from "./giveaways";
 
-export { TranscriptOptions, generateTranscript, Message } from "./transcripts";
+// transcript
+export { generateTranscript, TranscriptOptions, Message } from "./transcripts";
+
+// pagination
+export {
+    Button,
+    ButtonNames,
+    PaginationOptions,
+    pagination,
+} from "./pagination";
+
+// functions
+export { chatBot, ChatBotOptions } from "./functions/chatBot";
